8a45e5cc7fa6aa4a73a8e8e01f3013a9e066550f
andyogaga <andrewo@shleeq.com>
2020-12-17 12:48:33 +0100

    fixes channel room for chats

--- src/screens/room/room.component.js
+++ src/screens/room/room.component.js
@@ -6,37 +6,58 @@ import firestore from '@react-native-firebase/firestore';
 
 const Room = ({route}) => {
   const [messages, setMessages] = useState([]);
+  const [channel, setChannel] = useState('');
   const user = useSelector(({auth}) => auth.user);
   const {otherUser} = route.params;
 
   const myFirebaseCallback = useCallback(() => {
-    const unsubscribeListener = firestore()
-      .collection('threads')
-      .where('sender', '==', user.uid)
-      .where('receiver', '==', otherUser.id)
-      .orderBy('createdAt', 'desc')
-      .onSnapshot((querySnapshot) => {
-        if (querySnapshot) {
-          const myMessages =
-            querySnapshot &&
-            querySnapshot.docs.map((doc) => {
-              const data = {
-                _id: doc.id,
-                text: doc._data && doc._data.message ? doc._data.message : '',
-                createdAt: new Date().getTime(),
-              };
-              data.user = {
-                _id: doc._data && doc._data.sender,
-                avatar: 'https://placeimg.com/140/140/any',
-              };
-              return data;
+    let unsubscribeListener;
+    firestore()
+      .collection('channels')
+      .where('users', 'array-contains', user.uid, otherUser.id)
+      .get()
+      .then((response) => {
+        if (
+          response &&
+          response._docs &&
+          response._docs[0] &&
+          response._docs[0].id
+        ) {
+          setChannel(
+            response &&
+              response._docs &&
+              response._docs[0] &&
+              response._docs[0].id,
+          );
+          unsubscribeListener = firestore()
+            .collection('channels')
+            .where('channel', '==', channel)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot((querySnapshot) => {
+              if (querySnapshot) {
+                const myMessages =
+                  querySnapshot &&
+                  querySnapshot.docs.map((doc) => {
+                    const data = {
+                      _id: doc.id,
+                      text:
+                        doc._data && doc._data.message ? doc._data.message : '',
+                      createdAt: new Date().getTime(),
+                    };
+                    data.user = {
+                      _id: doc._data && doc._data.sender,
+                      avatar: 'https://placeimg.com/140/140/any',
+                    };
+                    return data;
+                  });
+                setMessages(myMessages);
+              }
             });
-          setMessages(myMessages);
         }
       });
 
     return () => unsubscribeListener();
-  }, [otherUser.id, user.uid]);
+  }, [otherUser.id, user.uid, channel]);
 
   useEffect(() => {
     myFirebaseCallback();
@@ -49,8 +70,7 @@ const Room = ({route}) => {
         .add({
           createdAt: new Date(),
           message: newMessages[0].text,
-          receiver: otherUser.id,
-          sender: user.uid,
+          channel,
         })
         .then(() => {
           setMessages((previousMessages) =>
@@ -58,7 +78,7 @@ const Room = ({route}) => {
           );
         });
     },
-    [otherUser.id, user.uid],
+    [channel],
   );
 
   return (
